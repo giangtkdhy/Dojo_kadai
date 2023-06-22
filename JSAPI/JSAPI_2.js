3bf044434cfdf6182fc8e6cce9f9bdfb3486747d
giangtkdhy <135286393+giangtkdhy@users.noreply.github.com>
2023-06-22 14:48:58 +0900

    kadai7 2回目

--- JSAPI/JSAPI_2.js
+++ JSAPI/JSAPI_2.js
@@ -1,33 +1,27 @@
 (function() {
   
   "use strict";
-  var products = {
-    "kintone": "KN",
-    "Garoon": "GR",
-    "サイボウズ Office": "OF",
-    "Mailwise": "MW"
-  };
-  var dateCell = "日付";
-  var productCell = "サイボウズ製品";
-  var numCell = "管理番号";
-  var dupCellCode = "重複禁止項目_文字列";
-
+  const products = { "kintone": "KN", "Garoon": "GR", "サイボウズ Office": "OF", "Mailwise": "MW" };
+  const productCell = "サイボウズ製品";
+  const numCell = "管理番号";
+  const dupCellCode = "重複禁止項目_文字列";
+// dateFnsライブラリー
   function formatDate(date) {
-    return dayjs(date).format("YYYYMMDD");
+    return dateFns.format(date, "YYYYMMDD");
   }
 
   kintone.events.on(["app.record.create.change." + productCell, "app.record.edit.change." + productCell,
     "app.record.create.change." + numCell, "app.record.edit.change." + numCell
   ], function(event) {
-    var rec = event.record;
-    var product = rec[productCell].value;
-    var num = rec[numCell].value;
-    var dupField = rec[dupCellCode];
- //重複チェック製品名と管理番号の両方が入力されている場合
+    var record = event.record;
+    var product = record[productCell].value;
+    var num = record[numCell].value;
+    var dupField = record[dupCellCode];
+
     if (product && num) {
-      var formattedDate = formatDate(dayjs());
-      var productOmit = products[product];
-      var formatValue = formattedDate + "-" + productOmit + "-" + num;
+      const formattedDate = formatDate(new Date());
+      const productOmit = products[product];
+      const formatValue = formattedDate + "-" + productOmit + "-" + num;
       dupField.value = formatValue;
  // subject 変数の定義: productField と numField の値を使用して、クエリ文字列を作成
       var subject = productCell + '="' + product + '" and ' + numCell + '="' + num + '"';
@@ -46,5 +40,15 @@
       dupCell.error = null;
     }
     return event;
+  }
+
+  kintone.events.on(["app.record.create.show", "app.record.edit.show"], function(event) {
+    const record = event.record;
+    record[dupCellCode].disabled = true; //重複禁止項目_文字列フィールドを編集不可にする
+    return event;
   });
+
+  kintone.events.on(["app.record.create.change." + productCell, "app.record.edit.change." + productCell,
+    "app.record.create.change." + numCell, "app.record.edit.change." + numCell
+  ], checkDup);
 })();
