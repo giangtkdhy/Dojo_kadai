2403fbf162cafc3b6fa7cb3bcedfe7517ad34366
giangtkdhy <135286393+giangtkdhy@users.noreply.github.com>
2023-06-27 10:50:56 +0900

    kadai9 3回目

--- RESTAPI/kadai9.js
+++ RESTAPI/kadai9.js
@@ -1,30 +1,24 @@
 (function() {
-  "use strict";
-  const products = { "kintone": "KN", "Garoon": "GR", "サイボウズ Office": "OF", "Mailwise": "MW" };
-  const productCell = "サイボウズ製品";
-  const numCell = "管理番号";
-  const dupCell = "重複禁止項目";
-  // 重複チェック関数
-  async function checkDup(value) {
-    const query = `${dupCell} = "${value}"`;
-    const params = { app: kintone.app.getId(), query: query };
-    const response = await kintone.api(kintone.api.url('/k/v1/records'), 'GET', params);
-    return response.records.length > 0;
-  }
-  // レコード保存時のイベントで呼び出される関数
-  kintone.events.on(["app.record.create.submit", "app.record.edit.submit"], async function(event) {
-    const record = event.record;
-    const product = record[productCell].value;
-    const num = record[numCell].value;
-    const dupField = record[dupCell];
-    // 重複チェック
-    const duplicateProhibitedItem = record[dupCell].value;
-    const isDuplicate = await checkDup(duplicateProhibitedItem);
-    if (isDuplicate) {
+    "use strict";
+    const dupCell = "重複禁止項目";
+    // 重複チェック関数
+    async function checkDup(value) {
+      const query = `${dupCell} = "${value}"`;
+      const params = { app: kintone.app.getId(), query: query };
+      const response = await kintone.api(kintone.api.url('/k/v1/records'), 'GET', params);
+      return response.records.length > 0;
+    }
+    // レコード保存時のイベントで呼び出される関数
+    kintone.events.on(["app.record.create.submit", "app.record.edit.submit"], async function(event) {
+      const duplicateProhibitedItem = event.record[dupCell].value;
+      // 重複チェック
+      const isDuplicate = await checkDup(duplicateProhibitedItem);
+      if (!isDuplicate) {
+        return event;
+      }
       const confirmed = confirm("レコードが重複しています。このまま保存しますか？");
       if (!confirmed) {
         return false;
       }
-    }
-  });
-})();
+    });
+  })();
